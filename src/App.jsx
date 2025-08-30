a0e90151bad9d0781ecbfe83f6a273af86e65ec2
R-Islombek <islombekraximjanov97dev@gmail.com>
2025-08-31 00:59:22 +0500

    birinchi commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+         <form  >
+              <label >Ismingizni kiriting</label>
+              <input type="text" placeholder='Ismingizni kiriting' />
+              <label >Familyangizni kiriting</label>
+              <input type="text" placeholder='Familyangizni kiriting' />
+         </form>
+    </>
+  )
+}
+
+export default App
